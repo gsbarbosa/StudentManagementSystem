9a29187ec13338c74b4de81098c129184eea5d44
gsbarbosa <gsbarbosa180@gmail.com>
2024-01-30 11:21:42 -0300

    Add project files.

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+    }
+
+}
---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+    }
+}
